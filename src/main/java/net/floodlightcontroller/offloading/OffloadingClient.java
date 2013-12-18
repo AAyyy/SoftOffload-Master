ac3e81d80b8fe4850de45ba013294229158d13de
Yanhe <yanhe.liu@cs.helsinki.fi>
2013-12-18 15:31:55 +0200

    Fix bugs caused by error hash key values; Replace tabs with 4 spaces;

--- src/main/java/net/floodlightcontroller/offloading/OffloadingClient.java
+++ src/main/java/net/floodlightcontroller/offloading/OffloadingClient.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package net.floodlightcontroller.offloading;
 
@@ -13,106 +13,106 @@ import net.floodlightcontroller.util.MACAddress;
  *
  */
 public class OffloadingClient implements Comparable<Object> {
-	private final MACAddress hwAddress;
-	private InetAddress ipAddress;
-	private float rate;
-	
+    private final MACAddress hwAddress;
+    private InetAddress ipAddress;
+    private float rate;
 
-	/**
-	 * construct a client instance
-	 * 
-	 * @param hwAddress Client's hw address
-	 * @param ipv4Address Client's IPv4 address
-	 */
-	public OffloadingClient(MACAddress hwAddress, InetAddress ipAddress) {
-		this.hwAddress = hwAddress;
-		this.ipAddress = ipAddress;
-	}
-	
-	/**
-	 * construct a client instance
-	 * 
-	 * @param hwAddress Client's hw address
-	 * @param ipv4Address Client's IPv4 address
-	 */
-	public OffloadingClient(String hwAddress, String ipAddress) throws UnknownHostException {
-		this.hwAddress = MACAddress.valueOf(hwAddress);
-		this.ipAddress = InetAddress.getByName(ipAddress);
-	}
-	
-	/**
-	 * Get the client's MAC address.
-	 * @return
-	 */
-	public MACAddress getMacAddress() {
-		return this.hwAddress;
-	}
 
-	/**
-	 * Get the client's IP address.
-	 * @return
-	 */
-	public InetAddress getIpAddress() {
-		return ipAddress;
-	}
-	
-	/**
-	 * Set the client's IP address
-	 * @param addr
-	 */
-	public void setIpAddress(InetAddress addr) {
-		this.ipAddress = addr;
-	}
-	
-	/**
-	 * Set the client's IP address
-	 * @param String addr
-	 * @throws UnknownHostException 
-	 */
-	public void setIpAddress(String addr) throws UnknownHostException {
-		this.ipAddress = InetAddress.getByName(addr);
-	}
-	
-	/**
-	 * get client's rate value
-	 * @return
-	 */
-	public float getRate() {
-		return this.rate;
-	}
-	
-	/**
-	 * Set the client's rate value
-	 * @param r
-	 */
-	public void updateRate(float r) {
-		this.rate = r;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (!(obj instanceof OffloadingClient))
-			return false;
+    /**
+     * construct a client instance
+     *
+     * @param hwAddress Client's hw address
+     * @param ipv4Address Client's IPv4 address
+     */
+    public OffloadingClient(MACAddress hwAddress, InetAddress ipAddress) {
+        this.hwAddress = hwAddress;
+        this.ipAddress = ipAddress;
+    }
 
-		if (obj == this)
-			return true;
-		
-		OffloadingClient that = (OffloadingClient) obj;
-			
-		return (this.hwAddress.equals(that.hwAddress));
-	}
+    /**
+     * construct a client instance
+     *
+     * @param hwAddress Client's hw address
+     * @param ipv4Address Client's IPv4 address
+     */
+    public OffloadingClient(String hwAddress, String ipAddress) throws UnknownHostException {
+        this.hwAddress = MACAddress.valueOf(hwAddress);
+        this.ipAddress = InetAddress.getByName(ipAddress);
+    }
 
-	
-	@Override
-	public int compareTo(Object o) {
-		assert (o instanceof OffloadingClient);
-		
-		if (this.hwAddress.toLong() == ((OffloadingClient)o).hwAddress.toLong())
-			return 0;
-		
-		if (this.hwAddress.toLong() > ((OffloadingClient)o).hwAddress.toLong())
-			return 1;
-		
-		return -1;
-	}
+    /**
+     * Get the client's MAC address.
+     * @return
+     */
+    public MACAddress getMacAddress() {
+        return this.hwAddress;
+    }
+
+    /**
+     * Get the client's IP address.
+     * @return
+     */
+    public InetAddress getIpAddress() {
+        return ipAddress;
+    }
+
+    /**
+     * Set the client's IP address
+     * @param addr
+     */
+    public void setIpAddress(InetAddress addr) {
+        this.ipAddress = addr;
+    }
+
+    /**
+     * Set the client's IP address
+     * @param String addr
+     * @throws UnknownHostException
+     */
+    public void setIpAddress(String addr) throws UnknownHostException {
+        this.ipAddress = InetAddress.getByName(addr);
+    }
+
+    /**
+     * get client's rate value
+     * @return
+     */
+    public float getRate() {
+        return this.rate;
+    }
+
+    /**
+     * Set the client's rate value
+     * @param r
+     */
+    public void updateRate(float r) {
+        this.rate = r;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof OffloadingClient))
+            return false;
+
+        if (obj == this)
+            return true;
+
+        OffloadingClient that = (OffloadingClient) obj;
+
+        return (this.hwAddress.equals(that.hwAddress));
+    }
+
+
+    @Override
+    public int compareTo(Object o) {
+        assert (o instanceof OffloadingClient);
+
+        if (this.hwAddress.toLong() == ((OffloadingClient)o).hwAddress.toLong())
+            return 0;
+
+        if (this.hwAddress.toLong() > ((OffloadingClient)o).hwAddress.toLong())
+            return 1;
+
+        return -1;
+    }
 }
--- src/main/java/net/floodlightcontroller/offloading/OffloadingProtocolServer.java
+++ src/main/java/net/floodlightcontroller/offloading/OffloadingProtocolServer.java
@@ -1,6 +1,6 @@
 /**
 *    Copyright 2013 University of Helsinki
-* 
+*
 *    Licensed under the Apache License, Version 2.0 (the "License"); you may
 *    not use this file except in compliance with the License. You may obtain
 *    a copy of the License at
@@ -30,110 +30,110 @@ import org.slf4j.LoggerFactory;
 
 /**
  * This is an implementation of sdn wireless controllers
- * 
+ *
  * @author Yanhe Liu <yanhe.liu@cs.helsinki.fi>
- * 
+ *
  **/
 
 class OffloadingProtocolServer implements Runnable {
 
-	protected static Logger log = LoggerFactory.getLogger(OffloadingProtocolServer.class);
+    protected static Logger log = LoggerFactory.getLogger(OffloadingProtocolServer.class);
 
-	// Message types
-	private final String MSG_CLIENT_INFO = "client";
-	private final String MSG_AGENT_RATE = "agentrate";
-	private final String MSG_CLIENT_RATE = "clientrate";
+    // Message types
+    private final String MSG_CLIENT_INFO = "client";
+    private final String MSG_AGENT_RATE = "agentrate";
+    private final String MSG_CLIENT_RATE = "clientrate";
 
-	private final int SERVER_PORT;
-	
-	private DatagramSocket controllerSocket;
-	private final ExecutorService executor;
-	private final OffloadingMaster offloadingMaster;
+    private final int SERVER_PORT;
 
-	public OffloadingProtocolServer (OffloadingMaster om, int port, ExecutorService executor) {
-		this.offloadingMaster = om; 
-		this.SERVER_PORT = port;
-		this.executor = executor;
-	}
-	
-	@Override
-	public void run() {
-		
-		try {
-			controllerSocket = new DatagramSocket(SERVER_PORT);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		while(true)	{
-			
-			try {
-				final byte[] receiveData = new byte[1024]; // We can probably live with less
-				final DatagramPacket receivedPacket = new DatagramPacket(receiveData, receiveData.length);
-				controllerSocket.receive(receivedPacket);
-				
-				executor.execute(new ConnectionHandler(receivedPacket));
-			}
-			catch (IOException e) {
-				log.error("controllerSocket.accept() failed: " + SERVER_PORT);
-				e.printStackTrace();
-				System.exit(-1);
-			}
-		}
-	}
-	
-	/** Protocol handlers **/
-	
-	private void receiveClientInfo(final InetAddress agentAddr, 
-			final String clientEthAddr, final String clientIpAddr) {
-		offloadingMaster.receiveClientInfo(agentAddr, clientEthAddr, clientIpAddr);
-	}
-	
-	private void receiveAgentRate(final InetAddress agentAddr, 
-			final String rate) {
-		offloadingMaster.receiveAgentRate(agentAddr, rate);
-	}
-	
-	private void receiveClientRate(final InetAddress agentAddr, 
-			final String clientEthAddr, final String clientIpAddr, 
-			final String clientRate) {
-		offloadingMaster.receiveClientRate(agentAddr, clientEthAddr, 
-				clientIpAddr, clientRate);
-	}
-	
-	private class ConnectionHandler implements Runnable {
-		final DatagramPacket receivedPacket;
-		
-		public ConnectionHandler(final DatagramPacket dp) {
-			receivedPacket = dp;
-		}
-		
-		// Agent message handler
-		public void run() {			
-			final String msg = new String(receivedPacket.getData()).trim().toLowerCase();
-			final String[] fields = msg.split(" ");
-			final String msg_type = fields[0];
-			final InetAddress agentAddr = receivedPacket.getAddress();
-            
-            if (msg_type.equals(MSG_CLIENT_INFO)) {
-            	final String clientEthAddr = fields[1];
-            	final String clientIpAddr = fields[2];
-            	
-            	receiveClientInfo(agentAddr, clientEthAddr, clientIpAddr);
-            	
-            } else if (msg_type.equals(MSG_AGENT_RATE)) {
-            	final String agentRate = fields[1];
+    private DatagramSocket controllerSocket;
+    private final ExecutorService executor;
+    private final OffloadingMaster offloadingMaster;
 
-            	receiveAgentRate(agentAddr, agentRate);
-            } else if (msg_type.equals(MSG_CLIENT_RATE)) {
-            	final String clientEthAddr = fields[1];
-            	final String clientIpAddr = fields[2];
-            	final String clientRate = fields[3];
+    public OffloadingProtocolServer (OffloadingMaster om, int port, ExecutorService executor) {
+        this.offloadingMaster = om;
+        this.SERVER_PORT = port;
+        this.executor = executor;
+    }
 
-            	receiveClientRate(agentAddr, clientEthAddr, clientIpAddr, clientRate);
+    @Override
+    public void run() {
+
+        try {
+            controllerSocket = new DatagramSocket(SERVER_PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while(true)	{
+
+            try {
+                final byte[] receiveData = new byte[1024]; // We can probably live with less
+                final DatagramPacket receivedPacket = new DatagramPacket(receiveData, receiveData.length);
+                controllerSocket.receive(receivedPacket);
+
+                executor.execute(new ConnectionHandler(receivedPacket));
             }
-            
-		}
-	}
+            catch (IOException e) {
+                log.error("controllerSocket.accept() failed: " + SERVER_PORT);
+                e.printStackTrace();
+                System.exit(-1);
+            }
+        }
+    }
+
+    /** Protocol handlers **/
+
+    private void receiveClientInfo(final InetAddress agentAddr,
+            final String clientEthAddr, final String clientIpAddr) {
+        offloadingMaster.receiveClientInfo(agentAddr, clientEthAddr, clientIpAddr);
+    }
+
+    private void receiveAgentRate(final InetAddress agentAddr,
+            final String rate) {
+        offloadingMaster.receiveAgentRate(agentAddr, rate);
+    }
+
+    private void receiveClientRate(final InetAddress agentAddr,
+            final String clientEthAddr, final String clientIpAddr,
+            final String clientRate) {
+        offloadingMaster.receiveClientRate(agentAddr, clientEthAddr,
+                clientIpAddr, clientRate);
+    }
+
+    private class ConnectionHandler implements Runnable {
+        final DatagramPacket receivedPacket;
+
+        public ConnectionHandler(final DatagramPacket dp) {
+            receivedPacket = dp;
+        }
+
+        // Agent message handler
+        public void run() {
+            final String msg = new String(receivedPacket.getData()).trim().toLowerCase();
+            final String[] fields = msg.split(" ");
+            final String msg_type = fields[0];
+            final InetAddress agentAddr = receivedPacket.getAddress();
+
+            if (msg_type.equals(MSG_CLIENT_INFO)) {
+                final String clientEthAddr = fields[1];
+                final String clientIpAddr = fields[2];
+
+                receiveClientInfo(agentAddr, clientEthAddr, clientIpAddr);
+
+            } else if (msg_type.equals(MSG_AGENT_RATE)) {
+                final String agentRate = fields[1];
+
+                receiveAgentRate(agentAddr, agentRate);
+            } else if (msg_type.equals(MSG_CLIENT_RATE)) {
+                final String clientEthAddr = fields[1];
+                final String clientIpAddr = fields[2];
+                final String clientRate = fields[3];
+
+                receiveClientRate(agentAddr, clientEthAddr, clientIpAddr, clientRate);
+            }
+
+        }
+    }
 
 }

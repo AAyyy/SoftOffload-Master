fd56121166844e163e1da276c38cfce23fc61694
Yanhe <yanhe.liu@cs.helsinki.fi>
2014-04-30 14:51:54 +0300

    modify OFMonitor download threshold value

--- src/main/java/net/floodlightcontroller/mobilesdn/OFMonitor.java
+++ src/main/java/net/floodlightcontroller/mobilesdn/OFMonitor.java
@@ -73,7 +73,7 @@ public class OFMonitor implements Runnable {
     private List<SwitchOutQueue> swQueueList;
 
     // default max rate threshold
-    static private final float RATE_THRESHOLD = 500000;
+    static private final float RATE_THRESHOLD = 1000000;
 
     // monitoring info is gathered by using a timer
     private class OFMonitorTask extends TimerTask {
@@ -138,7 +138,7 @@ public class OFMonitor implements Runnable {
                         float downrate = (receiveBytes - swQueue.getReceiveBytes()) / (this.interval);
                         // float uprate = (transmitBytes - swQueue.getTransmitBytes()) / (this.interval);
 
-                        if (downrate > 1000000) {
+                        if (downrate*8 > 1500000) {
                            int num = swQueue.getDownThroughputOverNum();
                            swQueue.setDownThroughputOverNum(++num);
                         } else {
